d868e2a99201ab5f7978a9db16aa918858269138
Ethan Wright <wrightet707@gmail.com>
2021-01-21 10:46:47 -0800

    install front end dependencies, build out app file

---
+++ app.js
@@ -0,0 +1,19 @@
+const mongoose =  require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
